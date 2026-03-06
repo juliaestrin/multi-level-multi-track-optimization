c213693a17fd0e02a6da7e196fa349cc93f1056d
qijia <qijia@Serein>
2026-03-06 14:16:25 -0500

    Update the datasheet

--- Switch Functions/try_with_functions.m
+++ Switch Functions/try_with_functions.m
@@ -1,6 +1,6 @@
 out1 = analyzePriSwitches(6.25e3, 500e3, 21.4142, 2, 10, ...
     [1 2 3 4 5 6 7 8], [1 2 3 4 5 6 7 8], 500, ...
-    'GaN Data.xlsx', 'SiC Data.xlsx');
+    'GaN Data tf.xlsx', 'SiC Data tf.xlsx');
 %
 % Secondary Side
 out2 = analyzeSecSwitches(6.25e3, 1000e3, 1, 10, [4 6 8], [4 6], 'Sec Data.xlsx');
